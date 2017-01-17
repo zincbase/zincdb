8331d4e1f7739dd3f779fb750515b409f8237b8e
rotemdan <rotemdan@gmail.com>
2017-01-17 22:18:06 +0200

    Added UTF8 encoding support to OmniString

--- lib/3_Library/Encoding/OmniString.ts
+++ lib/3_Library/Encoding/OmniString.ts
@@ -1,25 +1,30 @@
 namespace ZincDB {
 	export namespace Encoding {
 		export namespace OmniString {
-			export const encode = function(input: any): string {
+			export const encode = function (input: any): string {
 				if (input instanceof Uint8Array)
 					return "BB64:" + Base64.encode(input)
+				else if (typeof input === "string")
+					return "UTF8:" + input;
 				else
-					return "JSON:" + ZincDB.Tools.stringifyJSONOrUndefined(input);				
+					return "JSON:" + ZincDB.Tools.stringifyJSONOrUndefined(input);
 			}
 
-			export const decode = function(input: string): any {
+			export const decode = function (input: string): any {
 				if (input == null)
 					return input;
 
 				const valuePrefix = input.substr(0, 5);
+				const valuePayload = input.substr(5);
 
-				if (valuePrefix === "JSON:")
-					return ZincDB.Tools.parseJSONOrUndefined(input.substr(5))
+				if (valuePrefix === "UTF8:")
+					return valuePayload;
+				else if (valuePrefix === "JSON:")
+					return ZincDB.Tools.parseJSONOrUndefined(valuePayload)
 				else if (valuePrefix === "BB64:")
-					return Base64.decode(input.substr(5))
+					return Base64.decode(valuePayload)
 				else
-					throw new Error("Unsupported value found");				
+					throw new Error(`Encountered a value with an unsupported encoding '${valuePrefix.substr(0, 4)}`);
 			}
 		}
 	}
