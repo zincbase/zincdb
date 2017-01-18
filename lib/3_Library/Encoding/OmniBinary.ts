e4dfa5b36630ea0b1395985e36b9e3902766f7df
rotemdan <rotemdan@gmail.com>
2017-01-18 10:09:16 +0200

    Changed encodings internal to WebSQL, SQLite and LevelDB adapters

--- lib/3_Library/Encoding/OmniBinary.ts
+++ lib/3_Library/Encoding/OmniBinary.ts
@@ -1,35 +1,43 @@
 namespace ZincDB {
 	export namespace Encoding {
 		export namespace OmniBinary {
-			const enum SerializedValueType { Binary = 0, UTF8 = 1, JSON = 2 };
+			const enum EncodingIndex { Binary = 0, UTF8 = 1, JSON = 2, Base64 = 3 };
+
 			const encodingIdentifiers = [
-				new Uint8Array([SerializedValueType.Binary]),
-				new Uint8Array([SerializedValueType.UTF8]),
-				new Uint8Array([SerializedValueType.JSON])
+				new Uint8Array([82, 66, 73, 78]), // "RBIN"
+				new Uint8Array([85, 84, 70, 56]), // "UTF8"
+				new Uint8Array([74, 83, 79, 78]), // "JSON"
+				new Uint8Array([66, 66, 54, 52]), // "BB64"
 			];
 
 			export const encode = function (input: any): Uint8Array {
 				if (input instanceof Uint8Array) {
-					return ArrayTools.concatUint8Arrays([encodingIdentifiers[SerializedValueType.Binary], input]);
+					return ArrayTools.concatUint8Arrays([encodingIdentifiers[EncodingIndex.Binary], input]);
 				} else if (typeof input === "string") {
-					return ArrayTools.concatUint8Arrays([encodingIdentifiers[SerializedValueType.UTF8], Encoding.UTF8.encode(input)]);
+					return ArrayTools.concatUint8Arrays([encodingIdentifiers[EncodingIndex.UTF8], Encoding.UTF8.encode(input)]);
 				} else {
-					return ArrayTools.concatUint8Arrays([encodingIdentifiers[SerializedValueType.JSON], Encoding.UTF8.encode(ZincDB.Tools.stringifyJSONOrUndefined(input))]);
+					return ArrayTools.concatUint8Arrays([encodingIdentifiers[EncodingIndex.JSON], Encoding.UTF8.encode(ZincDB.Tools.stringifyJSONOrUndefined(input))]);
 				}
 			}
 
 			export const decode = function (input: Uint8Array): any {
-				const format: SerializedValueType = input[0];
-				const payload = input.subarray(1);
+				const payload = input.subarray(4);
 
-				if (format === SerializedValueType.Binary) {
-					return payload;
-				} else if (format === SerializedValueType.UTF8) {
-					return Encoding.UTF8.decode(payload);
-				} else if (format === SerializedValueType.JSON) {
+				const hasEncoding = (index: EncodingIndex) => {
+					const identifierBytes = encodingIdentifiers[index];
+					return input[0] === identifierBytes[0] && input[1] === identifierBytes[1] && input[2] === identifierBytes[2] && input[3] === identifierBytes[3];
+				}
+
+				if (hasEncoding(EncodingIndex.JSON)) {
 					return ZincDB.Tools.parseJSONOrUndefined(Encoding.UTF8.decode(payload));
+				} else if (hasEncoding(EncodingIndex.UTF8)) {
+					return Encoding.UTF8.decode(payload);
+				} else if (hasEncoding(EncodingIndex.Binary)) {
+					return payload;
+				} else if (hasEncoding(EncodingIndex.Base64)) {
+					return Encoding.Base64.decode(Encoding.UTF8.decode(payload));
 				} else
-					throw new TypeError(`Encunterened an unsupported input with type identifer ${format}`);
+					throw new TypeError(`Encunterened an unsupported input with type identifer ${Encoding.UTF8.decode(input.subarray(0, 4))}`);
 			}
 		}
 	}
--- lib/3_Library/Encoding/OmniString.ts
+++ lib/3_Library/Encoding/OmniString.ts
@@ -3,28 +3,28 @@ namespace ZincDB {
 		export namespace OmniString {
 			export const encode = function (input: any): string {
 				if (input instanceof Uint8Array)
-					return "BB64:" + Base64.encode(input)
+					return "BB64" + Base64.encode(input)
 				else if (typeof input === "string")
-					return "UTF8:" + input;
+					return "UTF8" + input;
 				else
-					return "JSON:" + ZincDB.Tools.stringifyJSONOrUndefined(input);
+					return "JSON" + ZincDB.Tools.stringifyJSONOrUndefined(input);
 			}
 
 			export const decode = function (input: string): any {
 				if (input == null)
 					return input;
 
-				const valuePrefix = input.substr(0, 5);
-				const valuePayload = input.substr(5);
+				const valuePrefix = input.substr(0, 4);
+				const valuePayload = input.substr(4);
 
-				if (valuePrefix === "UTF8:")
+				if (valuePrefix === "UTF8")
 					return valuePayload;
-				else if (valuePrefix === "JSON:")
+				else if (valuePrefix === "JSON")
 					return ZincDB.Tools.parseJSONOrUndefined(valuePayload)
-				else if (valuePrefix === "BB64:")
+				else if (valuePrefix === "BB64")
 					return Base64.decode(valuePayload)
 				else
-					throw new Error(`Encountered a value with an unsupported encoding '${valuePrefix.substr(0, 4)}`);
+					throw new Error(`Encountered a value with an unsupported encoding '${valuePrefix}`);
 			}
 		}
 	}
