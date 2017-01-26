a56e2075816c8252d5e354ef214c25c3d7f15d32
rotemdan <rotemdan@gmail.com>
2017-01-26 12:09:52 +0200

    (Resolves #65) WebStorageAdapter Added support for transactional writes with roll-back on error

--- lib/5_DB/LocalDB/Storage Adapters/WebStorageAdapter.ts
+++ lib/5_DB/LocalDB/Storage Adapters/WebStorageAdapter.ts
@@ -1,7 +1,12 @@
 namespace ZincDB {
 	export namespace DB {
-		type DatabaseMetadataEntry = { objectStoreNames?: string[] };
 		type MetadataAndValue = { metadata: EntryMetadata, value: any };
+
+		type DatabaseMetadataEntry = { objectStoreNames?: string[] };
+
+		// Rollback record structure:
+		// [Object store name, key, previous raw value (or null)]
+		type RollbackRecord = [string, string, string | null][];
 
 		export class WebStorageAdapter implements StorageAdapter {
 			private opened = false;
@@ -23,6 +28,12 @@ namespace ZincDB {
 			}
 
 			async open(): Promise<void> {
+				try {
+					this.rollbackIfNeeded();
+				} catch (e) {
+					throw new Error(`WebStorageAdapter Fatal error: couldn't roll-back a previous incomplete transaction. Reason: ${createErrorMessage(e)}`);
+				}
+
 				this.opened = true;
 			}
 
@@ -79,25 +90,65 @@ namespace ZincDB {
 			//////////////////////////////////////////////////////////////////////////////////////
 			// Write operations
 			//////////////////////////////////////////////////////////////////////////////////////
-			async set(transactionObject: { [objectStoreName: string]: { [key: string]: Entry<any> } }): Promise<void> {
+			async set(transactionObject: { [objectStoreName: string]: { [key: string]: Entry<any> | null } }): Promise<void> {
 				const objectStoreNames = await this.getObjectStoreNames();
 
 				for (const objectStoreName in transactionObject)
 					if (objectStoreNames.indexOf(objectStoreName) === -1)
 						throw new Error(`Object store '${objectStoreName}' does not exist.`)
 
+				// Create a roll-back record for this transaction
+				// containing the existing raw values for the keys that would be modified.
+				// At the same time, store the corresponding new values in a separate array.
+				const newValues: (Entry<any> | null)[] = [];
+				const rollbackRecord: RollbackRecord = [];
+
 				for (const objectStoreName in transactionObject) {
-					const operationsForObjectstore = transactionObject[objectStoreName];
+					const operationsForObjectStore = transactionObject[objectStoreName];
 
-					for (const key in operationsForObjectstore) {
-						const entry = operationsForObjectstore[key];
-
-						if (entry == null)
-							this.deleteKey(key, objectStoreName);
-						else
-							this.putRawValue(key, this.encodeEntryMetadataAndValue(entry), objectStoreName);
+					for (const key in operationsForObjectStore) {
+						rollbackRecord.push([objectStoreName, key, this.getRawValue(key, objectStoreName)]);
+						newValues.push(operationsForObjectStore[key]);
 					}
 				}
+
+				// Store the roll-back record
+				this.putRollbackRecord(rollbackRecord);
+
+				// Update the database with the new values, shortcut to use the roll-back record
+				// And the previously stored value list instead of re-scanning the transaction object
+				try {
+					for (let i = 0; i < rollbackRecord.length; i++) {
+						const [objectStoreName, key] = rollbackRecord[i];
+						const newValue = newValues[i];
+
+						if (newValue == null)
+							this.deleteKey(key, objectStoreName);
+						else
+							this.putRawValue(key, this.encodeEntryMetadataAndValue(newValue), objectStoreName);
+					}
+				}
+				catch (e) {
+					// If an error occured while writing the transaction, roll-back all changes,
+					// If the roll-back itself fails, the roll-back record would remain in storage
+					// and would be applied the next time the database is opened.
+					this.rollbackIfNeeded();
+
+					// Rethrow the error
+					throw e;
+				}
+
+				// If the execution arrived here, it means that all data has been successfuly written to
+				// WebStorage.
+				//
+				// For the special case of LocalStorage (SessionStorage is most likely memory-only):
+				//
+				// If the underlying engine for LocalStorage treats function boundaries as transactions, 
+				// it would mean that the data would be stored atomically on disk, otherwise, it is not
+				// in practice 100% safe to clear the roll-back record, but there isn't much that can be done as
+				// saving arbitrary length transaction logs would risk reaching the storage quota
+				// of the browser.
+				this.clearRollbackRecord();
 			}
 
 			//////////////////////////////////////////////////////////////////////////////////////
@@ -192,7 +243,104 @@ namespace ZincDB {
 			}
 
 			//////////////////////////////////////////////////////////////////////////////////////
-			// Private methods
+			// Low-level read operations (private)
+			//////////////////////////////////////////////////////////////////////////////////////
+			private getEntry(key: string, objectStoreName: string): Entry<any> {
+				const rawValue = this.getRawValue(key, objectStoreName);
+				if (rawValue == null)
+					return <any>undefined;
+
+				return { key, ...this.decodeValueAndMetadata(rawValue) };
+			}
+
+			private getRawValue(key: string, objectStoreName: string | undefined): string | null {
+				return this.webStorage.getItem(this.encodeToRawKey(key, objectStoreName));
+			}
+
+			private getAllWebStorageKeys(): string[] {
+				const webStorageLength = this.webStorage.length;
+				const allKeys: string[] = [];
+
+				for (let i = 0; i < webStorageLength; i++) {
+					allKeys.push(<string>this.webStorage.key(i));
+				}
+
+				return allKeys;
+			}
+
+			//////////////////////////////////////////////////////////////////////////////////////
+			// Low-level write operations (private)
+			//////////////////////////////////////////////////////////////////////////////////////				
+			private putRawValue(key: string, rawValue: string, objectStoreName: string | undefined): void {
+				this.webStorage.setItem(this.encodeToRawKey(key, objectStoreName), rawValue);
+			}
+
+			private deleteKey(key: string, objectStoreName: string | undefined): void {
+				this.webStorage.removeItem(this.encodeToRawKey(key, objectStoreName));
+			}
+
+			//////////////////////////////////////////////////////////////////////////////////////
+			// Metadata operations (private)
+			//////////////////////////////////////////////////////////////////////////////////////
+			private getDatabaseMetadata(): DatabaseMetadataEntry {
+				if (!this.isOpen)
+					throw new Error("Database is not open");
+
+				const encodedMetadata = this.getRawValue("@metadata", undefined);
+
+				if (encodedMetadata) {
+					return Encoding.OmniJson.decode(encodedMetadata);
+				} else {
+					return {};
+				}
+			}
+
+			private putDatabaseMetadata(newMetadata: DatabaseMetadataEntry): void {
+				if (!this.isOpen)
+					throw new Error("Database is not open");
+
+				this.putRawValue("@metadata", Encoding.OmniJson.encode(newMetadata), undefined);
+			}
+
+			//////////////////////////////////////////////////////////////////////////////////////
+			// Roll-back operations (private)
+			//////////////////////////////////////////////////////////////////////////////////////
+			private rollbackIfNeeded() {
+				const rollbackRecord = this.getRollbackRecord();
+
+				if (!rollbackRecord)
+					return;
+
+				rollbackRecord.forEach(([objectStoreName, key, rawValue]) => {
+					if (rawValue === null)
+						this.deleteKey(key, objectStoreName);
+					else
+						this.putRawValue(key, rawValue, objectStoreName);
+				});
+
+				log(`Rolled back a previous incomplete transaction in database '${this.dbName}'. Rollback record: ${Encoding.JsonX.encode(rollbackRecord)}`);
+				this.clearRollbackRecord();
+			}
+
+			private getRollbackRecord(): RollbackRecord | undefined {
+				const rawRollBackEntry = this.getRawValue("@rollback", undefined);
+
+				if (rawRollBackEntry == null)
+					return undefined;
+				else
+					return Encoding.JsonX.decode(rawRollBackEntry);
+			}
+
+			private putRollbackRecord(rollbackEntry: RollbackRecord): void {
+				this.putRawValue("@rollback", Encoding.JsonX.encode(rollbackEntry), undefined);
+			}
+
+			private clearRollbackRecord() {
+				this.deleteKey("@rollback", undefined);
+			}
+
+			//////////////////////////////////////////////////////////////////////////////////////
+			// Encoding operations (private)
 			//////////////////////////////////////////////////////////////////////////////////////
 			private getObjectStorePrefix(objectStoreName: string | undefined) {
 				if (objectStoreName == null)
@@ -215,57 +363,6 @@ namespace ZincDB {
 				const encodedValue = encodedMetadataAndValue.substring(firstTabIndex + 1);
 
 				return { value: Encoding.OmniJson.decode(encodedValue), metadata: Encoding.OmniJson.decode(encodedMetadata) };
-			}
-
-			private getEntry(key: string, objectStoreName: string): Entry<any> {
-				const rawValue = this.getRawValue(key, objectStoreName);
-				if (rawValue == null)
-					return <any> undefined;
-				
-				return { key, ...this.decodeValueAndMetadata(rawValue) };				
-			}
-
-			private getRawValue(key: string, objectStoreName: string | undefined): string | null {
-				return this.webStorage.getItem(this.encodeToRawKey(key, objectStoreName));
-			}
-
-			private putRawValue(key: string, value: string, objectStoreName: string | undefined): void {
-				this.webStorage.setItem(this.encodeToRawKey(key, objectStoreName), value);
-			}
-
-			private deleteKey(key: string, objectStoreName: string | undefined): void {
-				this.webStorage.removeItem(this.encodeToRawKey(key, objectStoreName));
-			}
-
-			private getAllWebStorageKeys(): string[] {
-				const webStorageLength = this.webStorage.length;
-				const allKeys: string[] = [];
-
-				for (let i = 0; i < webStorageLength; i++) {
-					allKeys.push(<string>this.webStorage.key(i));
-				}
-
-				return allKeys;
-			}
-
-			private getDatabaseMetadata(): DatabaseMetadataEntry {
-				if (!this.isOpen)
-					throw new Error("Database is not open");
-
-				const encodedMetadata = this.getRawValue("@metadata", undefined);
-
-				if (encodedMetadata) {
-					return Encoding.OmniJson.decode(encodedMetadata);
-				} else {
-					return {};
-				}
-			}
-
-			private putDatabaseMetadata(newMetadata: DatabaseMetadataEntry): void {
-				if (!this.isOpen)
-					throw new Error("Database is not open");
-
-				this.putRawValue("@metadata", Encoding.OmniJson.encode(newMetadata), undefined);
 			}
 
 			//////////////////////////////////////////////////////////////////////////////////////
