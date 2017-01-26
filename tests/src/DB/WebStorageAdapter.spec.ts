591fe98b062f1d89b44af09f02b037662ff35803
rotemdan <rotemdan@gmail.com>
2017-01-26 14:11:57 +0200

    (Resolves #67) Added a dedicated test for WebStorageAdapter roll-back behaviort

---
+++ tests/src/DB/WebStorageAdapter.spec.ts
@@ -0,0 +1,62 @@
+namespace ZincDB {
+	export namespace DB {
+		if (WebStorageAdapter.isAvailable) {
+			const dbName = "webStorageAdapterTestSuite-" + JSRandom.getWordCharacterString(10);
+
+			describe(`WebStorage adapter rollback test`, () => {
+				it("Automatically rolls back all changes if a transaction fails because it causes session storage to exceed the quota", async () => {
+					const storage = new WebStorageAdapter(dbName, "SessionStorage");
+					storage.open();
+
+					await storage.createObjectStoresIfNeeded(["store1", "store2"]);
+					await storage.set({
+						"store1": {
+							"key0": { key: "key0", value: "good", metadata: { updateTime: 0 } },
+							"key1": { key: "key1", value: 1234, metadata: { updateTime: 1 } },
+							"key2": { key: "key2", value: "hello", metadata: { updateTime: 2 } }
+						},
+						"store2": {
+							"key3": { key: "key3", value: 4321, metadata: { updateTime: 3 } },
+							"key4": { key: "key4", value: "world", metadata: { updateTime: 4 } }
+						}
+					});
+
+					// Create a very long string of length 100 * 100000 = ~10MB
+					let longStringComponents: string[] = []
+					for (let i = 0; i < 100000; i++)
+						longStringComponents.push("LongStringLongStringLongStringLongStringLongStringLongStringLongStringLongStringLongStringLongString");
+					const veryLongString = longStringComponents.join("");
+
+					// Try to execute the transaction
+					try {
+						await storage.set({
+							"store1": {
+								"key1": { key: "key1", value: 9999, metadata: { updateTime: 11 } },
+								"key2": null,
+							},
+							"store2": {
+								"key3": { key: "key3", value: 1111, metadata: { updateTime: 12 } },
+								"key4": { key: "key4", value: veryLongString, metadata: { updateTime: 13 } }
+							}
+						});
+					}
+					catch (e) {
+						// Possible error names: 
+						// "QuotaExceededError" (IE/Edge/Chrome)
+						// "NS_ERROR_DOM_QUOTA_REACHED" (Firefox)
+
+						expect(await storage.get("key0", "store1")).toEqual({ key: "key0", value: "good", metadata: { updateTime: 0 } });
+						expect(await storage.get("key1", "store1")).toEqual({ key: "key1", value: 1234, metadata: { updateTime: 1 } });
+						expect(await storage.get("key2", "store1")).toEqual({ key: "key2", value: "hello", metadata: { updateTime: 2 } });
+						expect(await storage.get("key3", "store2")).toEqual({ key: "key3", value: 4321, metadata: { updateTime: 3 } });
+						expect(await storage.get("key4", "store2")).toEqual({ key: "key4", value: "world", metadata: { updateTime: 4 } });
+
+						return;
+					}
+
+					expect(true).toBe(false, "Expected transaction to fail with a QuotaExceededError");
+				});
+			});
+		}
+	}
+}
